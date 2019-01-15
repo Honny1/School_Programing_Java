e9a3f97fcbac28649ae226af830cd1c187d2826b
Hony Rodák <hony.com@seznam.cz>
2019-01-15 08:33:23 +0100

    15-1-2019

--- stackWithArrayList/src/hony/god/Main.java
+++ stackWithArrayList/src/hony/god/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         MyStack<Object> stack = new MyStack<>();
+
+        System.out.println("Hodnota při odebírání z prázdného stacku Odebrano: "+(String) stack.pop());
+        System.out.println(stack);
+
         for(int i =0 ; i<100;i++){
             stack.push(i);
         }
@@ -13,5 +17,5 @@ public class Main {
         System.out.println("Odebrano: "+(String) stack.pop());
         System.out.println(stack);
     }
-    }
+
 }
--- stackWithArrayList/src/hony/god/MyStack.java
+++ stackWithArrayList/src/hony/god/MyStack.java
@@ -1,4 +1,35 @@
 package hony.god;
 
-public class MyStack {
+import java.util.ArrayList;
+
+public class MyStack<E> {
+    private ArrayList<E> list;
+
+
+    public MyStack(){
+        this.list=new ArrayList<E>();
+    }
+
+    public void push(E value) {
+        this.list.add(value);
+    }
+
+    public E pop(){
+        if (!this.list.isEmpty()){
+        E item =this.list.get(this.list.size()-1);
+        this.list.remove(this.list.size()-1);
+        return item;
+        }else {
+            return null;
+        }
+    }
+
+    public String toString() {
+        String out = "Stack: { ";
+        for (int i = this.list.size()-1; i > 0; i--) {
+            out += this.list.get(i).toString() + ",";
+        }
+        out += "}";
+        return out;
+    }
 }
