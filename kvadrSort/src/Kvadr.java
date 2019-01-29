04bc2620a4a7ace41a2343e8bed7c1cfbca74162
Hony Rodák <hony.com@seznam.cz>
2019-01-29 08:28:00 +0100

    29/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/201929/1/2019

---
+++ kvadrSort/src/Kvadr.java
@@ -0,0 +1,41 @@
+public class Kvadr implements Comparable<Kvadr> {
+    private  int a;
+    private  int b;
+    private  int c;
+
+    public Kvadr(int a,int b,int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return this.a;
+    }
+
+    public int getB() {
+        return this.b;
+    }
+
+    public int getC() {
+        return this.c;
+    }
+
+    public int getVolume(){
+        return this.getA()*this.getB()*this.getC();
+    }
+    @Override
+    public String toString() {
+        String out= String.format("Kvádr :{a= %s, b= %s, c= %s, objem= %s }", this.getA(), this.getB(), this.getC(),this.getVolume());
+        return out;
+    }
+
+    @Override
+    public int compareTo(Kvadr o) {
+        if  (this.getVolume() == o.getVolume())
+            return 0;
+        if (this.getVolume() > o.getVolume())
+            return 1;
+        return -1;
+    }
+}
---
+++ kvadrSort/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Kvadr> k= new ArrayList<Kvadr>();
+        k.add(new Kvadr(10,20,19));
+        k.add(new Kvadr(11,24,12));
+        k.add(new Kvadr(12,25,16));
+        k.add(new Kvadr(13,26,1));
+
+        System.out.println("Original list: " +k);
+
+        System.out.println("by AGE:");
+        Collections.sort(k);
+        System.out.println("Sorted list: "+k);
+    }
+}
